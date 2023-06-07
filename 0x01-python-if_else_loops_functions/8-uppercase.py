69f777c0cb8a8da614ad4acced14ebd5ae7ab9b1
idrissjuanito <idrissjuanito@gmail.com>
2023-06-07 16:03:07 +0000

    task08: char to ascii conversion 02

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
         numChar = ord(c)
         if (numChar > 96 and numChar < 123):
             numChar -= 32
-        print(chr(numChar), end="")
+        print("{}".format(chr(numChar)), end="")
     print()
