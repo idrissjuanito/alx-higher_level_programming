6a0e89679c9258a1c1d0319a0b6338a53f35984b
idrissjuanito <idrissjuanito@gmail.com>
2023-06-07 20:14:58 +0000

    task13: adding number to sorted linked list 02

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -25,7 +25,7 @@ listint_t *insert_node(listint_t **head, int number)
 		current = new;
 		return (new);
 	}
-	while (current->next && current->next->n < number)
+	while (current->n < number && current->next)
 		current = current->next;
 	new->next = current->next;
 	if (current->n > number)
