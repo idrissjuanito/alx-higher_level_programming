7f4126c8ef67b39d05bb5a7ea4911e39f80531f7
idrissjuanito <idrissjuanito@gmail.com>
2023-06-07 15:30:04 +0000

    correction on nested loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(9):
     for j in range(i+1, 10):
         if (i < 8):
-           print("{0}{1}".format(i, j), end=", ") 
+            print("{0}{1}".format(i, j), end=", ")
         else:
-           print("{0}{1}".format(i, j)) 
+            print("{0}{1}".format(i, j))
