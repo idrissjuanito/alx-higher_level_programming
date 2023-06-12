4ec9a012704bd34a51e30768bd5ebc606ed1b89e
idrissjuanito <idrissjuanito@gmail.com>
2023-06-12 17:36:38 +0000

    correction task 02

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if not isinstance(my_list, list):
-        return my_list
-
-    if not element:
-        return my_list
     if idx < 0 or idx > len(my_list):
         return my_list
     my_list[idx] = element
