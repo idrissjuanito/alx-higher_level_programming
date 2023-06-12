d6a047835212e9f476baf1a594e2f7dbb8b66ac9
idrissjuanito <idrissjuanito@gmail.com>
2023-06-12 09:10:50 +0000

    correction 4 on command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ from sys import argv, exit
 
 if __name__ != "__main__":
     exit(1)
-pluralize = "s" if len(argv) > 2 else ""
+pluralize = "s" if len(argv) != 2 else ""
 punct = "." if len(argv) < 2 else ":"
 print("{} argument{}{}".format(len(argv) - 1, pluralize, punct))
 
