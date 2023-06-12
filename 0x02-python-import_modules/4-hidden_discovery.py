ea5163c298614d42e35b035de494f7d4f336e491
idrissjuanito <idrissjuanito@gmail.com>
2023-06-12 11:44:13 +0000

    task 04 edge case correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 
 if __name__ == "__main__":
     import hidden_4
-    for n in dir(hidden_4):
+    mod_content = sorted(dir(hidden_4))
+    for n in mod_content:
         if not n.startswith("__"):
             print(n)
