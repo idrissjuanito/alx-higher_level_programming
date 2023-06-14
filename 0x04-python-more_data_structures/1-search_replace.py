095fc12ec0c8709601ed7caf818a6de80e17f4af
idrissjuanito <idrissjuanito@gmail.com>
2023-06-14 14:14:40 +0000

    task 01 2

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = list()
+    if not my_list or not search:
+        return None
+    for item in my_list:
+       if item == search:
+           new_list.append(replace)
+           continue
+       new_list.append(item)
+    return new_list
