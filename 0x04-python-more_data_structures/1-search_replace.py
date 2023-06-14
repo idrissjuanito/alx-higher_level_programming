dd8955d30e5582069f10b1217f6d6538e428a104
idrissjuanito <idrissjuanito@gmail.com>
2023-06-14 14:19:59 +0000

    correction task 01 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,11 @@
 
 def search_replace(my_list, search, replace):
     new_list = list()
-    if not my_list or not search:
+    if not my_list or not search or not replace:
         return None
     for item in my_list:
-       if item == search:
-           new_list.append(replace)
-           continue
-       new_list.append(item)
-    return new_list
+        if item == search:
+            new_list.append(replace)
+            continue
+        new_list.append(item)
+        return new_list
