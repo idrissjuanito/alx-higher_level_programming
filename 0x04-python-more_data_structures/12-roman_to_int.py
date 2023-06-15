5d6e23f7d06edaa30ebfdf56a800c225fab996bf
idrissjuanito <idrissjuanito@gmail.com>
2023-06-15 14:37:06 +0000

    correction on task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,11 +6,16 @@ def roman_to_int(roman_string):
     roman_val = [1, 5, 10, 50, 100, 500, 1000]
     if not roman_string or type(roman_string) is not str:
         return num
+    prev_val = None
     for i in range(len(roman_string)):
         r = roman_string[i]
-        if r in romans:
-            if r == "X" and roman_string[i - 1] == "I":
-                num += 8
-                continue
-            num += roman_val[romans.index(r)]
+        idx_val = romans.index(r)
+        val = roman_val[idx_val]
+
+        if prev_val and val > prev_val:
+            num += val - prev_val * 2
+            prev_val = None
+            continue
+        num += val
+        prev_val = val
     return num
