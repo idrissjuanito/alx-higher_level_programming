c327af8b52911f75243a5be9ef53a17458bfedb7
idrissjuanito <idrissjuanito@gmail.com>
2023-06-15 08:48:53 +0000

    a couple of corrections

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
+    sum = 0
     if not my_list:
-        return None
+        return sum
     if len(my_list) < 1:
         return sum
     new_list = sorted(my_list)
     last_num = None
-    sum = 0
     for n in new_list:
         if not n == last_num:
             sum += n
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return None
     new_set = set()
+    if not set_1 or not set_2:
+        return set()
     for s in set_1:
         for ss in set_2:
             if s == ss:
