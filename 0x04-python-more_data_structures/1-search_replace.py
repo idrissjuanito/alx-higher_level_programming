c5dad5c200f4c67f891ff1eec671611b95d3d85e
idrissjuanito <idrissjuanito@gmail.com>
2023-06-15 08:51:18 +0000

    correct test case task 01

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,9 +3,9 @@
 def search_replace(my_list, search, replace):
     new_list = list()
     if not my_list:
-        return None
+        return new_list
     if len(my_list) < 1:
-        return None
+        return new_list
     for item in my_list:
         if item == search:
             new_list.append(replace)
