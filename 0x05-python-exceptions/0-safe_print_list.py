c138af67252960cfd0164aca435cc52963010e8d
idrissjuanito <idrissjuanito@gmail.com>
2023-06-26 19:06:26 +0000

    Fix attempt checker 8 task 00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if (x == 0 or not isinstance(my_list, list)):
-        return 0
     for i in range(x):
         try:
             print(my_list[i], end="")
