9a8f0b21d00757b83c53877c4efd7c89089642f5
idrissjuanito <idrissjuanito@gmail.com>
2023-06-26 20:50:48 +0000

    attempt fix checker task 04

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            count += 1
         except IndexError:
-            break
-    if (count > 0)
-        print()
+            if (i > 0):
+                print()
+            return i
+    print()
     return count
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = list()
-    if (list_length == 0):
-        return new_list
-    if (not isinstance(my_list_1, list) or not isinstance(my_list_2, list)):
-        return new_list
     for i in range(list_length):
         try:
             quot = my_list_1[i] / my_list_2[i]
