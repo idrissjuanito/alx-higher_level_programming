1bad73924fbebfd737715122ef7b505d544b9d97
idrissjuanito <idrissjuanito@gmail.com>
2023-06-26 20:39:27 +0000

    attempt to fix checker task 00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             count += 1
         except IndexError:
-            pass
+            break
     if (count > 0)
         print()
     return count
