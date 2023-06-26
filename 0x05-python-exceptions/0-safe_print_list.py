911a56a2dc43ed641b9b2aeae68c14287b1a1d78
idrissjuanito <idrissjuanito@gmail.com>
2023-06-26 18:58:45 +0000

    Fix: edge case on task 00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
         except IndexError:
-            print()
+            if (i > 0):
+                print()
             return i
     print()
     return i+1
