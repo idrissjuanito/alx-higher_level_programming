32b712c89f6e7a0cb55959efe6eb021d7781c8ce
idrissjuanito <idrissjuanito@gmail.com>
2023-06-26 16:58:27 +0000

    answer to task 00

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    if (x == 0 or not isinstance(my_list, list)):
+        return 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            print()
+            return i
+    print()
+    return i+1
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,5 @@
+# ALX SE Project
+
+## Python module
+
+### Python exceptions and error handling
