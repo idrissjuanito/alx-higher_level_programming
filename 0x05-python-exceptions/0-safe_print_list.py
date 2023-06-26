cf05eb2631fad3f8e1ec0f045046c625895e352c
idrissjuanito <idrissjuanito@gmail.com>
2023-06-26 20:16:59 +0000

    attempt to fix checkers task 00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
+            count += 1
         except IndexError:
-            if (i > 0):
-                print()
-            return i
-    print()
-    return i+1
+            pass
+    if (count > 0)
+        print()
+    return count
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    if (x == 0 or not isinstance(my_list, list)):
-        return 0
     printed = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except TypeError:
-            pass
-        except ValueError:
-            pass
-        else:
             printed += 1
+        except (TypeError, ValueError):
+            pass
     print()
     return printed
