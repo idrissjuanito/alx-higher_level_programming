05d50eb6ecb1517d9eefecb9dfeac4090f637f3b
idrissjuanito <idrissjuanito@gmail.com>
2023-06-27 20:28:40 +0000

    task 01 python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Holds a Square class """
+
+
+class Square:
+    """ Square class to create square objects
+
+    Attributes:
+        size: size of the square
+    """
+    __size
