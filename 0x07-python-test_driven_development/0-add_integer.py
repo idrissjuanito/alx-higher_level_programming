ccadb30636f9a695b91fecc2d2cd097d4badf08b
idrissjuanito <idrissjuanito@gmail.com>
2023-07-01 09:26:52 +0000

    task 00 of TDD python TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    module holds function for addition
+"""
+def add_integer(a, b):
+    """
+        Returns the addition of two numbers
+        a (int): first number
+        b (int): second number
+    """
+    if not type(a) is int and not type(a) is float:
+        raise TypeError("a must be an integer")
+    if not type(b) is int and not type(b) is float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+# ALX SE Higher level programming in python
+
+Project on Test Driven Development
