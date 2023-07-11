42b0e90216d0140b6284938f0314f35fc9aa15ab
idrissjuanito <idrissjuanito@gmail.com>
2023-07-11 12:02:51 +0000

    task 01 python inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" module for list class """
+
+
+class MyList(list):
+    """ subclass of the list class
+        adds methods to the list class
+    """
+    def print_sorted(self):
+        """ sorts and prints a list """
+        self.sort()
+        print(self)
