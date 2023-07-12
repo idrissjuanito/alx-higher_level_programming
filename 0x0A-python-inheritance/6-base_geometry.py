1ed61a9be3934a2257973634d21d0282bb65cebf
idrissjuanito <idrissjuanito@gmail.com>
2023-07-12 15:16:30 +0000

    task 06 python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module with Geometry class """
+
+
+class BaseGeometry:
+    """ Class for a base geometry """
+    def area():
+        """ calculates the area of a shape"""
+        raise Exception("area() is not implemented")
