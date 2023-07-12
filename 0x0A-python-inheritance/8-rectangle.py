47006d89eea78861b4dab66023e98ecd0e92be7b
idrissjuanito <idrissjuanito@gmail.com>
2023-07-12 18:53:45 +0000

    task 08 inheritance correction

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ rectangle class module """
-BaseGeometry = __import__("base_geometry").BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -10,7 +10,7 @@ class Rectangle(BaseGeometry):
         height (int): height of the integer
     """
     def __init__(self, width, height):
-        self.integer_validator("Width", width)
-        self.integer_validator("Height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
