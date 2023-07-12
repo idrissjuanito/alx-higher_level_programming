bd5b54f8ed7e0ff14bc708290c7899b71169e7f9
idrissjuanito <idrissjuanito@gmail.com>
2023-07-12 09:55:13 +0000

    task 04 python inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module on python inheritance """
+
+
+def inherits_from(obj, a_class):
+    """ checks if obj is instance of class
+    Args:
+        obj (object): object to check
+        a_class: class to check against
+    Returns:
+        True if obj is related to a_class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
