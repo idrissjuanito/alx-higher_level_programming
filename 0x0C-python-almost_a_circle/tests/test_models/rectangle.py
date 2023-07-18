6180574a9514b78bfa67785d41a2fa2a65ca9daa
idrissjuanito <idrissjuanito@gmail.com>
2023-07-18 19:16:37 +0000

    tests python almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/rectangle.py
@@ -7,7 +7,8 @@ import unittest
 class TestRectangle(unittest.TestCase):
     def test_two_att(self):
         r1 = Rectangle(3, 5)
-        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 3)
+        self.assertEqual(r1.height, 5)
 
     def test_five_att(self):
         r2 = Rectangle(2, 4, 0, 1, 90)
