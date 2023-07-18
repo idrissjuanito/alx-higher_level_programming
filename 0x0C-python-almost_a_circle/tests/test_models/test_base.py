5ddb8dc0cf3e08f0b2c4566d68814bcea80daf54
idrissjuanito <idrissjuanito@gmail.com>
2023-07-18 12:11:47 +0000

    testing python almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,7 +2,6 @@
 """ test file for base class """
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
 
 class TestBase(unittest.TestCase):
     """ test unit for the base class – inherits unittest's TestCase class """
@@ -10,7 +9,5 @@ class TestBase(unittest.TestCase):
     def test_id(self):
         b = Base()
         self.assertEqual(b.id, 1)
-        b2 = Base()
+        b1 = Base()
         self.assertEqual(b2.id, 2)
-        b3 = Base(23)
-        self.assertEqual(b3.id, 23)
