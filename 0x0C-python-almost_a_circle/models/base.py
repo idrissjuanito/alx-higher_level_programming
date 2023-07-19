f6f8ab3c0cfe23f62d97c88797901667a7b42654
idrissjuanito <idrissjuanito@gmail.com>
2023-07-19 11:08:56 +0000

    correction task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,7 @@ class Base():
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ Converts a dictionary to its json equivalent"""
         if not list_dictionaries or len(list_dictionaries) == 0:
