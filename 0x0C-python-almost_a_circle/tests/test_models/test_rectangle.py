467d8c280378b118b268fb2eb46d8ce21b17d787
idrissjuanito <idrissjuanito@gmail.com>
2023-07-24 03:04:13 +0000

    test almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,6 +9,16 @@ class TestRectangle(unittest.TestCase):
         r2 = Rectangle(3, 4, 6)
         self.assertEqual(r2.x, 6)
 
+    def test_init_all(self):
+        rect = Rectangle(3, 2, 5, 8, 9)
+        self.asserEqual(rect.id, 9)
+
+    def test_string_x(self):
+        try:
+            rect2 = Rectangle(2, 3, "4")
+        except TypeError as e:
+            self.assertEqual(e.__class__.__name, "TypeError")
+
     def test_four_att(self):
         r3 = Rectangle(3, 2, 0, 2)
         self.assertEqual(r3.y, 2)
