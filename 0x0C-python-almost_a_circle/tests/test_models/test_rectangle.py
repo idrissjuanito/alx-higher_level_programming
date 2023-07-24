8b77da2056f87fa227ce41b1e88bf34bedca9ae1
idrissjuanito <idrissjuanito@gmail.com>
2023-07-24 02:01:17 +0000

    test almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,11 +7,7 @@ import unittest
 class TestRectangle(unittest.TestCase):
     def test_two_att(self):
         r1 = Rectangle(3, 5)
-        self.assertEqual(r1.width, 3)
-
-    def test_two_att(self):
-        r1 = Rectangle(3, 5)
-        self.assertEqual(r1.height, 5)
+        self.assertEqual(r1.id, 4)
 
     def test_three_att(self):
         r2 = Rectangle(3, 4, 6)
@@ -21,13 +17,9 @@ class TestRectangle(unittest.TestCase):
         r3 = Rectangle(3, 2, 0, 2)
         self.assertEqual(r3.y, 2)
 
-    def test_five_att(self):
-        r4 = Rectangle(2, 4, 0, 1, 90)
-        self.assertEqual(r4.id, 90)
-
     def test_string_height(self):
         try:
-            r4 = Rectangle(3, "2")
+            rec2 = Rectangle(3, "8")
         except TypeError as e:
             self.assertEqual(e.__str__(), "height must be an integer")
 
