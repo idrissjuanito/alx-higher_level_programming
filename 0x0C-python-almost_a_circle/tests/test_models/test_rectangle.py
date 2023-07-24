4d034eb20518451702c759d6101aa76dc9bc9c54
idrissjuanito <idrissjuanito@gmail.com>
2023-07-24 02:44:23 +0000

    tests unittest python

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,10 +5,6 @@ import unittest
 
 
 class TestRectangle(unittest.TestCase):
-    def test_two_att(self):
-        r1 = Rectangle(3, 5)
-        self.assertEqual(r1.id, 8)
-
     def test_three_att(self):
         r2 = Rectangle(3, 4, 6)
         self.assertEqual(r2.x, 6)
