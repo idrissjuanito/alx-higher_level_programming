6a72a80158197f052b253ef5e857bdbdf1450e33
idrissjuanito <idrissjuanito@gmail.com>
2023-07-24 01:03:23 +0000

    correction tests almost a circle python

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Tests for rectangle class """
-from models.base from Rectangle
+from models.rectangle import Rectangle
 import unittest
 
 
@@ -8,7 +8,7 @@ class TestRectangle(unittest.TestCase):
     def test_two_att(self):
         r1 = Rectangle(3, 5)
         self.assertEqual(r1.width, 3)
-        self.assertEqual(r1.height, 3)
+        self.assertEqual(r1.height, 5)
 
     def test_three_att(self):
         r2 = Rectangle(3, 4, 6)
@@ -20,4 +20,4 @@ class TestRectangle(unittest.TestCase):
 
     def test_five_att(self):
         r4 = Rectangle(2, 4, 0, 1, 90)
-        self.assertEqual(r4.y, y)
+        self.assertEqual(r4.y, 1)
