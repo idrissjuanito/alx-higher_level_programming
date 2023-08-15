a34d95cf344878c8c0cffefa594d45c7d1ecd166
idrissjuanito <idrissjuanito@gmail.com>
2023-08-15 23:05:30 +0100

    task 13: deleting records from table

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- updates a particular record in a table
+UPDATE second_table SET score = 10 WHERE name = "Bob";
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Deletes record with particular values
+DELETE FROM second_table WHERE score <= 5;
