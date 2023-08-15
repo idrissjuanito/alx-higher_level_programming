158695a3a36362660d3892679e0acc894dc3412f
idrissjuanito <idrissjuanito@gmail.com>
2023-08-16 00:14:57 +0100

    task100: correctioni 4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- convert a database and tables to charset ut8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8;
+ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
