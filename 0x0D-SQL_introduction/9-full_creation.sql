075abb764dc835718a49e85d1ee22edda242fbb9
idrissjuanito <idrissjuanito@gmail.com>
2023-08-15 22:48:02 +0100

    task 09: create table and insert records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- create a table and insert series of records in
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table values (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
