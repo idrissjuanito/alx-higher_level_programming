4f4ac38cfa37295113cf16fdd5ac01619d735a22
idrissjuanito <idrissjuanito@gmail.com>
2023-08-15 23:24:40 +0100

    task 14 to 16: aggregations and fliters

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- computes the average of scores
+SELECT AVG(score) AS average from second_table;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- List non null records
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
