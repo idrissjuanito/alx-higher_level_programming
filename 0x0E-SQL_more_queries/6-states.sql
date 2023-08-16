ee172230145a66a7df68aa85f05e4a69607141b6
idrissjuanito <idrissjuanito@gmail.com>
2023-08-16 17:45:26 +0000

    task06: setting auto increment field and primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creates a database and a table inside
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT UNIQUE,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY (`id`));
