2409cb89af18129999b4e89e1458687a932bfb1c
idrissjuanito <idrissjuanito@gmail.com>
2023-08-16 18:40:10 +0000

    task08: querying cities table for matching states in state table

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- query cities from a particular state from states table
-SELECT id, state_id, name FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id;
