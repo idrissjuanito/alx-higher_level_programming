389c3016c0bec101921996cffb115c8425f54c5f
idrissjuanito <idrissjuanito@gmail.com>
2023-08-16 19:23:44 +0000

    task11: left join two tables

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- query all tv shows and print null for genre_id if show has no genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
