4bff319bb527e3d46dae9279b095a5b4739a7a06
idrissjuanito <idrissjuanito@gmail.com>
2023-08-16 01:41:47 +0100

    task 102; correction 2

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- display partial info grouped by and filtered
-SELECT city, AVG(value) AS temp_avg FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY temp_avg DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
