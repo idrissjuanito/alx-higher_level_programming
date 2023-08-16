8ab3ea30f1df607f6d3a28a6f86969c04b99f77e
idrissjuanito <idrissjuanito@gmail.com>
2023-08-16 17:38:04 +0000

    task 05: create table with unique field value

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- creates a table if does not exist and make id field unique
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1 UNIQUE,
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE,
 name VARCHAR(256));
