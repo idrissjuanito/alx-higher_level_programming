7743fdb5145b6b6bf8fc5c1e1b24b7bc4c61376f
idrissjuanito <idrissjuanito@gmail.com>
2023-08-17 15:32:13 +0000

    task17: subquery with joins to three tables

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- query genres of a particular tv_show
+SELECT name
+FROM tv_genres
+WHERE NOT name IN (SELECT name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter")
+ORDER BY name;
