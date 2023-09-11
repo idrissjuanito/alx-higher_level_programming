0133ff944008f5b2f98cd7291b729506bfee1e0e
idrissjuanito <idrissjuanito@gmail.com>
2023-09-12 00:03:23 +0100

    task 02 script arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
+
 let output = 'No argument';
-if (argv.length === 2) {
+if (process.argv.length === 3) {
   output = 'Argument found';
 }
-if (argv.length > 2) {
+if (process.argv.length > 3) {
   output = 'Arguments found';
 }
 console.log(output);
