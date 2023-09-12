214f56abda7a880279b7b0762afeccc880747a28
idrissjuanito <idrissjuanito@gmail.com>
2023-09-12 09:45:57 +0100

    task 10 recursive function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 function factorial (n) {
-    let m = parseInt(n)
-    if (isNaN(m) || m === 1)
-        return 1;
-    return m * factorial(m - 1);
+  const m = parseInt(n);
+  if (isNaN(m) || m === 1) return 1;
+  return m * factorial(m - 1);
 }
 console.log(factorial(process.argv[2]));
