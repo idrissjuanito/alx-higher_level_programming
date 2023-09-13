222ca15c7bdebc2a02a809e136b14b1bbeaa767e
idrissjuanito <idrissjuanito@gmail.com>
2023-09-13 13:51:36 +0100

    task 102 file systems

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,10 +2,12 @@
 const fs = require('fs');
 
 const { argv } = process;
-fs.readFile(argv[2], (data) => {
+fs.readFile(argv[2], (err, data) => {
+  if (err) throw err;
   fs.writeFile(argv[4], data, () => {
-    fs.readFile(argv[3], (data2) => {
-      fs.appendFile(argv[4], data2);
+    fs.readFile(argv[3], (err, data2) => {
+      if (err) throw err;
+      fs.appendFile(argv[4], data2, () => {});
     });
   });
 });
