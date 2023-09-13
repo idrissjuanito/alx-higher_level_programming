cc42e79818ba4ecb65d7534bf1389f51be1e9c5b
idrissjuanito <idrissjuanito@gmail.com>
2023-09-13 13:58:29 +0100

    task 06 inheritance without new construtor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Square = require('./5-square');
+const Square1 = require('./5-square');
 
-module.exports = class Square extends Square {
-  constructor (size) {
-    super(size);
-  }
-
+module.exports = class Square extends Square1 {
   charPrint (c) {
     if (c === undefined) c = 'X';
     for (let i = 0; i < this.height; i++) {
