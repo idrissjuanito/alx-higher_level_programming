9c91480920d19fbfa8148c8ccf146f270137964b
idrissjuanito <idrissjuanito@gmail.com>
2023-09-15 16:52:15 +0100

    task 01 python ORM project

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module connect to MySQL using MySQLdb Module
+    – selects all states but filters by names starting with N using MySQL regex operator
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE states.name REGEXP '^N' ORDER BY states.id")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
