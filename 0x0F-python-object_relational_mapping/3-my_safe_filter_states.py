32a9cb5e437821f6a022a74246ef50beb3312079
idrissjuanito <idrissjuanito@gmail.com>
2023-09-16 11:48:30 +0100

    task 3 sanitize args for sql query

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Module connect to MySQL using MySQLdb Module
+    – selects all states and filters by the fouth
+    argument passed to script
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cursor = db.cursor()
+    query = '''SELECT * FROM states WHERE states.name LIKE BINARY %s ORDER BY states.id'''
+    cursor.execute(query, (argv[4],))
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
