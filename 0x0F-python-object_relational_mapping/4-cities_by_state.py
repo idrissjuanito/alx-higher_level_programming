df07057d00b79e40499715deed5ac285d7f1a3f0
idrissjuanito <idrissjuanito@gmail.com>
2023-09-16 12:22:21 +0100

    task 4 queries with joins on MySQLdb python orm

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Module connect to MySQL using MySQLdb Module
+    – selects all states and filters by the fouth
+    argument passed to script
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cursor = db.cursor()
+    query = '''SELECT cities.id, cities.name, states.name
+                FROM cities LEFT JOIN states
+                ON cities.state_id = states.id ORDER BY cities.id'''
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
