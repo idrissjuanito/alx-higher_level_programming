3668a08d11de285be5dbcf59d293c909985e0036
idrissjuanito <idrissjuanito@gmail.com>
2023-09-18 00:38:24 +0100

    task 13 – deleting db items sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Deleting rows in database """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+from sqlalchemy.orm.unitofwork import DeleteAll
+from model_state import Base, State
+
+if __name__ == "__main__":
+    con_str = "mysql+mysqldb://{}:{}@localhost/{}".format(
+                argv[1], argv[2], argv[3])
+    engine = create_engine(con_str)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id)
+    for row in res:
+        session.delete(row)
+    session.commit()
