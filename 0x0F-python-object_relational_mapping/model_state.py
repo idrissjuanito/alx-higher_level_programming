05863f27686e9d75fb612caf3e62ecaa9bf66f3f
idrissjuanito <idrissjuanito@gmail.com>
2023-09-17 05:18:58 +0100

    sqlalchemy orm class 2

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" Object Relational database manipulation """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.functions import char_length
 
 Base = declarative_base()
 
 
 class State(Base):
+    """ creates table states using sqlalchemy
+        and table columns: id and name
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True,
                 unique=True, nullable=False, autoincrement=True)
