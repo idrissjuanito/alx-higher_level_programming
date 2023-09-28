e89fd8c9a67e474856e96b05b19410a94918ca57
idrissjuanito <idrissjuanito@gmail.com>
2023-09-28 20:17:14 +0100

    peak of python list try 3

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,13 +2,12 @@
 """Looks for peaks in a list"""
 def find_peak(list_of_integers):
     """ Finds and returns a peak from given list """
-    llen = len(list_of_integers)
     peakindex = 0
-    if llen < 1: return None
-    for i in range(1, llen):
+    if len(list_of_integers) < 1: return None
+    for i in range(1, len(list_of_integers)):
         if list_of_integers[i] < list_of_integers[i-1]:
             continue
-        if i + 1 == llen:
+        if i + 1 == len(list_of_integers):
             peakindex = i
             break
         if list_of_integers[i] == list_of_integers[i+1]:
