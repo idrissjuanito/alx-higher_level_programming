21ac7ed881823ca87d9a12d00d72639cc63508fa
idrissjuanito <idrissjuanito@gmail.com>
2023-09-29 23:17:07 +0100

    python urllib request with error handling

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" urllib request with error handling """
+from sys import argv
+from urllib import request, error
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print('Error Code:', e.code)
