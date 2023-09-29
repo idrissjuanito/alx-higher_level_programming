21a24d372a22273c3a2be52e8e118f666ff94fb3
idrissjuanito <idrissjuanito@gmail.com>
2023-09-30 00:44:47 +0100

    requests module sending params and receiving json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" sending request and parsing json response """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    val = argv[1] if len(argv) > 1 else ""
+    res = requests.post('http://0.0.0.0:5000/search_user', params={'q': val})
+    try:
+        r = res.json()
+        if len(r.keys()) < 1:
+            print('No result')
+        else:
+            print('[{}] {}'.format(r.id, r.name))
+    except requests.exceptions.InvalidJSONError as e:
+        print('Not a valid JSON')
+    print(res.text)
