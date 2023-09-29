df397231c53bfcc75dd6bc63e04618ab365bfda0
idrissjuanito <idrissjuanito@gmail.com>
2023-09-29 21:43:18 +0100

    urllib response headers extractions 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-""" using urllib for queries – getting request id from header """
 from urllib import request
 from sys import argv
-
 
 with request.urlopen(argv[1]) as res:
     print(res.headers['X-Request-Id'])
