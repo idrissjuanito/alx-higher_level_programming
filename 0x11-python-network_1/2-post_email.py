84a1a28ed7e487a58ecd08f3f15e1795a0e69c9c
idrissjuanito <idrissjuanito@gmail.com>
2023-09-29 22:45:20 +0100

    sending post request with data urllib python

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ send urllib request with some data """
 import sys
-from urllib import request
+import urllib.request
 
 if __name__ == "__main__":
-    req = request.Request(
+    req = urllib.request.Request(
             url=sys.argv[1],
-            data=sys.argv[2].encode('utf-8'),
+            data=urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii'),
             method='POST')
-    with request.urlopen(req) as res:
+    with urllib.request.urlopen(req) as res:
         print(res.read().decode('utf-8'))
