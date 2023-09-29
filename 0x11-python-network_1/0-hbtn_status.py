a6e4f9b98e2bb8c9257c4862ff34a5e2ddd550a3
idrissjuanito <idrissjuanito@gmail.com>
2023-09-29 21:26:09 +0100

    basic python requet with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,7 @@ from urllib import request
 
 with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read()
-    isutf8 = 'OK' if response.headers.get_content_charset() == 'utf-8' else ''
-    print('content:', body)
     print('Body response:')
     print('\t- type: {}'.format(type(body)))
-    print("\t- content: ", body)
-    print('\t- utf8 content: {}'.format(isutf8))
+    print("\t- content:", body)
+    print('\t- utf8 content: {}'.format(body.decode('utf-8')))
