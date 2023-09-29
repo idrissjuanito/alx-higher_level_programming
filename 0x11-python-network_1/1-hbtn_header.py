136148b85536402ff25ef22adfeff0c5b801e00f
idrissjuanito <idrissjuanito@gmail.com>
2023-09-29 22:04:18 +0100

    urllib response headers extractions

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,5 +3,6 @@
 import sys
 from urllib import request
 
-with request.urlopen(sys.argv[1]) as res:
-    print(res.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with request.urlopen(sys.argv[1]) as res:
+        print(res.headers.get('X-Request-Id'))
