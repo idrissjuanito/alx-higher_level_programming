c64462697fdf4666895144ddb04cd2cab2abcafd
idrissjuanito <idrissjuanito@gmail.com>
2023-09-29 23:18:47 +0100

    python urllib request with error handling

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
         with request.urlopen(argv[1]) as res:
             print(res.read().decode('utf-8'))
     except error.HTTPError as e:
-        print('Error Code:', e.code)
+        print('Error code:', e.code)
