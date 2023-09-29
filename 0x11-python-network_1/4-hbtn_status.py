44741395e4a57076eac2593eee7df4c7088bc7f8
idrissjuanito <idrissjuanito@gmail.com>
2023-09-29 23:32:14 +0100

    using the requests module to make http requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == "__main__":
     res = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('- type:', type(res.text))
-    print('- content:', res.text)
+    print('\t- type:', type(res.text))
+    print('\t- content:', res.text)
