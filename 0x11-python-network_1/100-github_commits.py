d388f9097b2ea7d522b3ae0471fda5a3e6f076b9
idrissjuanito <idrissjuanito@gmail.com>
2023-09-30 02:00:06 +0100

    query github api for commits by a repo and owner 3

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     heads = {'X-GitHub-Api-Version': '2022-11-28',
              'Accept': 'application/vnd.github+json'}
     url = f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits'
-    res = requests.get(url, headers=heads)
+    res = requests.get(url, headers=heads, params={'per_page': 10})
     r = res.json()
     if len(r) != 0:
         for commit in r:
