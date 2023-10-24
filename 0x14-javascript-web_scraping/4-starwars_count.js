61abe4902264248ecf2e1d256c58de742fb0f2f3
idrissjuanito <idrissjuanito@gmail.com>
2023-10-24 14:01:04 +0100

    parsing json data with node

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,17 @@
 // Api query with request library
 const request = require('request');
 const { argv } = require('process');
-const characterLink = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(argv[2], (error, res, body) => {
   if (error) console.log(error);
   body = JSON.parse(body);
-  let films = body.results;
-  films = films.filter(film => film.characters.includes(characterLink));
-  console.log(films.length);
+  const films = body.results;
+  const num = films.reduce((prev, next) => {
+    const characters = next.characters;
+    for (let i = 0; i < characters.length; i++) {
+      if (characters[i].slice(-3, -1) === '13') prev++;
+    }
+    return prev;
+  }, 0);
+  console.log(num);
 });
